a2f0381e02ca2cab6a503d2d3f105912a9ddfdfe
oiratey <oiratey@gmail.com>
2021-12-02 14:41:36 +0800

    add block ad in weibo

--- zhihu_font_and_others.user.js
+++ zhihu_font_and_others.user.js
@@ -114,6 +114,48 @@ large(['feed-item','zu-main-content-inner','Question-main','Post-RichTextContain
         }
     }
 
+//屏蔽新浪微博feed流中的广告
+//head_info_authtag_29zK2_s
+//head-info_authtag_29zK2
+//temp_s = wbpro-auth-tag head-info_authtag_29zK2
+    let hot_t_s = document.getElementsByClassName("wbpro-auth-tag head-info_authtag_29zK2");
+    if (hot_t_s!=null){
+        for (let i = 0; i < hot_t_s.length; i++){
+            if (hot_t_s[i].textContent == "热推"){
+                hot_t_s[i].parentNode.parentNode.parentNode.parentNode.parentNode.style.display="none";
+            }
+        }
+    }
+
+
+    let head_info_authtag_29zK2_s=document.getElementsByClassName("head-info_authtag_29zK2");
+    if (head_info_authtag_29zK2_s!=null){
+        for (let i = 0; i < head_info_authtag_29zK2_s.length; i++){
+            if ( head_info_authtag_29zK2_s[i].hasChildNodes("img") && head_info_authtag_29zK2_s[i].querySelector("img").hasAttribute("src")
+                && head_info_authtag_29zK2_s[i].querySelector("img").getAttribute("src") == "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA"+
+"ADgAAAAeCAYAAAB5c901AAACbUlEQVR4AeWYA5AdQRCGY9spxradQmwXYtsux0lxdrQ3sQqxbdssxLbN7trJ1flu92bv7m226j/0m/lff9O9mlT/1XHnzp1M58+f73/u3DkF2hJiUpg7"+
+"MsQId+nSpQpnz569CfoOOhdqgJizzv0mskSrHH4AAy9euHChZKh2IOaODMgSqZIQGID0oQwXERJZsF3Dg9i/WGKvpgsXLszDmDWPc57X7VylaPGVK2mOiLEFC1hRzq01nJOmXvLRp5iK"+
+"CLgF5RUQkunGGPkDv9e7n0suwNw3S5YsCW8p26ZV0U8IOtitX0QeY4A60U2YlJS0ccLn8Lp6YfRqJzMgtg1oS0yCFj0Bvx/h33GoVRS/lRpwN/ye/0/YCTp+RMeiSSlV2A/AD2D+DWDu"+
+"eRHM7xmhSrXA6xdU/gV8diOiIHZHAz7RsWjCc9cXQPjyk4m90h04cCAdeF3CxZJSlsaYwRZNfkDwGe5UiM7SoWABbt9OMwLcxNWrV2cOHKAQZATeFuISfMd7BAR9jnucJU0DfkksoG2z+"+
+"oxZ000IKtzJGOCCBaSgs6rWnlQp9/AOKITVQF+615hKBPy6gt8gt1JKpfcBkPZBQAC1zQFa19DTrYQQ2YwD4iVdX9kGmgTEp5g/f/6kTog4JzN8AwTj7WgeFkbLmQQE7XIxfrovgNCW9f"+
+"QF5jauZKAAndawTusLzDiMBQpQCNrLgSMvlFI5zQOSX/D7a0IEC/zTOCCYTnXak7XTIdMVvCEEGZYQ4XXAK2CcWxZS0jqG2bQv6SEl65Dw8bQO53QU3gddbVngBk2QN52Cv22oy1oxsB"+
+"u/wd+6D/DxF6S9dE9EL8SBAAAAAElFTkSuQmCC"){
+                head_info_authtag_29zK2_s[i].parentNode.parentNode.parentNode.parentNode.parentNode.style.display="none";
+            }
+        head_info_authtag_29zK2_s[i].style.width="900px";
+        }
+    }
+
+//    <div class="head-info_authtag_29zK2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAACbUlEQVR4AeWYA5AdQRCGY9spxradQmwXYtsux0lxdrQ3sQqxbdssxLbN7trJ1flu92bv7m226j/0m/lff9O9mlT/1XHnzp1M58+f73/u3DkF2hJiUpg7MsQId+nSpQpnz569CfoOOhdqgJizzv0mskSrHH4AAy9euHChZKh2IOaODMgSqZIQGID0oQwXERJZsF3Dg9i/WGKvpgsXLszDmDWPc57X7VylaPGVK2mOiLEFC1hRzq01nJOmXvLRp5iKCLgF5RUQkunGGPkDv9e7n0suwNw3S5YsCW8p26ZV0U8IOtitX0QeY4A60U2YlJS0ccLn8Lp6YfRqJzMgtg1oS0yCFj0Bvx/h33GoVRS/lRpwN/ye/0/YCTp+RMeiSSlV2A/AD2D+DWDueRHM7xmhSrXA6xdU/gV8diOiIHZHAz7RsWjCc9cXQPjyk4m90h04cCAdeF3CxZJSlsaYwRZNfkDwGe5UiM7SoWABbt9OMwLcxNWrV2cOHKAQZATeFuISfMd7BAR9jnucJU0DfkksoG2z+oxZ000IKtzJGOCCBaSgs6rWnlQp9/AOKITVQF+615hKBPy6gt8gt1JKpfcBkPZBQAC1zQFa19DTrYQQ2YwD4iVdX9kGmgTEp5g/f/6kTog4JzN8AwTj7WgeFkbLmQQE7XIxfrovgNCW9fQF5jauZKAAndawTusLzDiMBQpQCNrLgSMvlFI5zQOSX/D7a0IEC/zTOCCYTnXak7XTIdMVvCEEGZYQ4XXAK2CcWxZS0jqG2bQv6SEl65Dw8bQO53QU3gddbVngBk2QN52Cv22oy1oxsBu/wd+6D/DxF6S9dE9EL8SBAAAAAElFTkSuQmCC"></div>
+//    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAACbUlEQVR4AeWYA5AdQRCGY9spxradQmwXYtsux0lxdrQ3sQqxbdssxLbN7trJ1flu\
+//92bv7m226j/0m/lff9O9mlT/1XHnzp1M58+f73/u3DkF2hJiUpg7MsQId+nSpQpnz569CfoOOhdqgJizzv0mskSrHH4AAy9euHChZKh2IOaODMgSqZIQGID0oQwXERJZsF3Dg9i/WGK\
+//vpgsXLszDmDWPc57X7VylaPGVK2mOiLEFC1hRzq01nJOmXvLRp5iKCLgF5RUQkunGGPkDv9e7n0suwNw3S5YsCW8p26ZV0U8IOtitX0QeY4A60U2YlJS0ccLn8Lp6YfRqJzMgtg1oS0\
+//yCFj0Bvx/h33GoVRS/lRpwN/ye/0/YCTp+RMeiSSlV2A/AD2D+DWDueRHM7xmhSrXA6xdU/gV8diOiIHZHAz7RsWjCc9cXQPjyk4m90h04cCAdeF3CxZJSlsaYwRZNfkDwGe5UiM7So\
+//WABbt9OMwLcxNWrV2cOHKAQZATeFuISfMd7BAR9jnucJU0DfkksoG2z+oxZ000IKtzJGOCCBaSgs6rWnlQp9/AOKITVQF+615hKBPy6gt8gt1JKpfcBkPZBQAC1zQFa19DTrYQQ2YwD\
+//4iVdX9kGmgTEp5g/f/6kTog4JzN8AwTj7WgeFkbLmQQE7XIxfrovgNCW9fQF5jauZKAAndawTusLzDiMBQpQCNrLgSMvlFI5zQOSX/D7a0IEC/zTOCCYTnXak7XTIdMVvCEEGZYQ4XX\
+//AK2CcWxZS0jqG2bQv6SEl65Dw8bQO53QU3gddbVngBk2QN52Cv22oy1oxsBu/wd+6D/DxF6S9dE9EL8SBAAAAAElFTkSuQmCC"
+
+
 //屏蔽篱笆论坛帖子列表中的广告
     let ui_list_item_and_ui_list_merchant_ad_s=document.getElementsByClassName('ui-list-item ui-list-merchant-ad');
     if (ui_list_item_and_ui_list_merchant_ad_s!=null){
